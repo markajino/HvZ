c01902ba192f19c00524dade93239f80076b137a
Giorgos Markozanis <markajinob@gmail.com>
2023-03-24 18:46:01 +0200

    fixed map.js zoom issue - deleted bounds

--- src/features/Map.js
+++ src/features/Map.js
@@ -14,16 +14,12 @@ function Map({ width, height, lat, lng }) {
   });
 
   const center = {
-    lat: -3.745 || lat,
-    lng: -38.523 || lng,
+    lat: 37.9838 || lat,
+    lng: 23.7275 || lng,
   };
   const [map, setMap] = React.useState(null);
 
   const onLoad = React.useCallback(function callback(map) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-
     setMap(map);
   }, []);
 
